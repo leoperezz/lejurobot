6d31f1074242c3a3f441d6ebee1ba315a2507e26
leoperezz <leo.perez.nestares@gmail.com>
2026-02-03 16:04:21 -0500

    adding trainer file

---
+++ train.sh
@@ -0,0 +1,34 @@
+export CUDA_VISIBLE_DEVICES=1
+#export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+
+DATASET_REPO_ID="NONHUMAN-RESEARCH/pick-and-place-fruits_v2_cleaned"
+EXPERIMENT_NAME="flowact_training_fruits"
+PRETRAINED_PATH="lerobot/pi05_base"
+POLICY_TYPE="flowact"
+STEPS=20000
+
+#python xhuman/policies/pi05/train_val_pi05.py \
+#python scripts/train_val.py \
+python lejurobot/scripts/train_val.py \
+    --dataset.repo_id=${DATASET_REPO_ID} \
+    --policy.type=${POLICY_TYPE} \
+    --output_dir=./outputs/${EXPERIMENT_NAME} \
+    --job_name=${EXPERIMENT_NAME} \
+    --policy.repo_id=NONHUMAN-RESEARCH/${EXPERIMENT_NAME} \
+    --policy.pretrained_path=${PRETRAINED_PATH} \
+    --policy.compile_model=false \
+    --policy.gradient_checkpointing=true \
+    --policy.scheduler_decay_steps=${STEPS} \
+    --policy.num_inference_steps=5 \
+    --policy.optimizer_lr=1.5e-5 \
+    --policy.device=cuda \
+    --wandb.enable=true \
+    --wandb.project=XHUMAN \
+    --policy.dtype=bfloat16 \
+    --steps=${STEPS} \
+    --eval_freq=200 \
+    --log_freq=200 \
+    --split_ratio=0.9 \
+    --batch_size=4 \
+    --save_freq=500 \
